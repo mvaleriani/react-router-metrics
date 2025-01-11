771308f0fbfbbe678fe3518f95d1431d040b0d34
Yujie Zhu <29871135+y00j@users.noreply.github.com>
2025-01-11 11:56:04 -0800

    change index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import type { Route } from "./+types/home";
+import type { Route } from "./+types/_index.tsx";
 import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
